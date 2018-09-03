626d225b40bdfa3d9f0787956ba22da46367eb62
Palmer Paul <pzpaul2002@yahoo.com>
2018-09-03 15:53:28 -0400

    fix comments in Basics

--- Basics.hs
+++ Basics.hs
@@ -12,6 +12,7 @@ import Data.Char (toUpper)
 {-
 Let's start out by getting to know you a little bit.
 (And you'll get some practice using the basic Haskell data types too!)
+
 Think of each `undefined` as a todo. When you are finished with
 the assignment there should be no `undefined`s left in your code.
 -}
@@ -30,19 +31,22 @@ Okay, this next one is a little more interesting. The type annotation says that
 `githubUsername` is a `Maybe String`. What does that mean? Well not everyone
 has a GitHub account (although I encourage you to make one if you don't). How
 can our data represent that?
+
 A first idea would be to use an empty string for someone who doesn't have a
 GitHub account. This seems fine but a potential problem arises when I try to
-load your GitHub page by doing ``"https://github.com/" ++ githubUsername`.
+load your GitHub page by doing `"https://github.com/" ++ githubUsername`.
 Instead of getting your page I will get the homepage. Sure I can fix this by
 checking for empty strings whenever I use `githubUsername` but it is too easy
 to forget to do that, especially if you are working in a large code base
 dealing with code you didn't even write.
+
 Can we do better? Perhaps there is a way to make the compiler force us to deal
 with possibility of a missing GitHub account wherever `githubUsername` is used.
 That's where `Maybe` comes in. We are going to talk a lot about `Maybe` in
 lec3. For now just know that `Maybe` is a way to encode possible failure. If
 the value exists, we can wrap it with `Just`. For example, `Just 5` is a
 `Maybe Int`. In case no such value exists, we use `Nothing`.
+
 (If you are familiar with OCaml, in that language we would write this type as
 `string option` where `Just` and `Nothing` correspond to `Some` and `None`.)
 -}
@@ -58,10 +62,11 @@ githubUsername = undefined
 Let's write a function to display a full name. The function should just
 concatenate the first and last name given to it with a space in between.
 Here are some examples to get you started:
-    `fullName "Haskell" "Curry" == "Haskell Curry"`.
-    `fullName "" "NoFirstName" == " NoFirstName"`
-    `fullName "NoLastName" "" == "NoLastName "`
-    `fullName "" "" == ""`
+
+    fullName "Haskell" "Curry" == "Haskell Curry"
+    fullName "" "NoFirstName" == " NoFirstName"
+    fullName "NoLastName" "" == "NoLastName "
+    fullName "" "" == " "
 -}
 
 fullName :: String -> String -> String
@@ -71,7 +76,8 @@ fullName fname lname = undefined
 {-
 The registrar just informed me that they actually need us to display last names
 first. Particularly, the new format should be `lastName, firstName`.
-    `lastCommaFirst "George" "Washington" == "Washington, George"`
+
+    lastCommaFirst "George" "Washington" == "Washington, George"
 -}
 
 lastCommaFirst :: String -> String -> String
@@ -80,22 +86,28 @@ lastCommaFirst fname lname = undefined
 {-
 Alright, last one. This time we want to display initials, or the first letter
 of a person's first name concatenated with the first letter of their lastname.
-    `initials "Ada" "Lovelace" == "AL"`
+
+    initials "Ada" "Lovelace" == "AL"
+
 How can we get the first letter of a `String`? There's a standard library
 function `head` that returns the first element of whatever list you give it.
 But we are working with `String`s not lists-- how does that help us? Well it
 turns out that `String`s are just lists (and your whole life has been a lie).
 Specifically, `String` is just shorthand for a list of `Char`s (we call this
 a type alias in the biz).
+
 So now we can get the first letter of a `String` but something is wrong. If
 a `String` is a list of `Char`s and `head` gives us the first element of a list
 then in our case that element will have type `Char`. That's where
 `charToString` comes into play. It takes a single character and turns into a
 singleton character list a.k.a. a `String`!
+
 Oh, and one more thing! Make sure that `initials` always returns an uppercase
 string. You can use `toUpper` for that which takes a character and returns
 its uppercase version. Note that `toUpper` operates on `Char`s not `String`s!
-    `initials` "alan" "turing" == "AT"
+
+    initials "alan" "turing" == "AT"
+
 (Don't worry about what happens when either input is empty. Spoiler: it
 probably isn't good. We can talk about this in lec2.)
 -}
@@ -148,10 +160,12 @@ someOddNumbers = undefined : [ 3, 11, 77 ]
 {-
 By using multiple cons calls together we can build a list. `(:)` is
 "right associative" which means that
+
     1 : 9 : 4 : [] == 1 : (9 : (4 : []))
                    == 1 : (9 : [4])
                    == 1 : [9, 4]
                    == [1, 9, 4]
+
 But it is good style to omit the parentheses like so.
 -}
 
@@ -203,8 +217,10 @@ it is to use pattern matching. But for now we will use the projection functions
 `fst` and `snd`. As the names might suggest, `fst` takes a tuple of size two
 and returns the first component, while `snd` takes a tuple of size two and
 returns the second component.
+
 Let's use `fst` and `snd` to write the `addIntPairs` function which takes to
 two tuples of size two and adds them component wise to produce a new tuple.
+
     addIntPairs ( 1, 2 ) ( 3, 4 ) == ( 4, 6 )
     addIntPairs ( -1, 3 ) ( 2, 0 ) == ( 1, 3 )
 -}
